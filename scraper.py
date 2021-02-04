3eed4f27974d47d3b3e396a9a533422822f93471
sam <samuelclark907@gmail.com>
2021-02-03 19:37:13 -0900

    everything works

--- scraper.py
+++ scraper.py
@@ -18,8 +18,24 @@ def get_citations_needed_report(enter_url):
     response = requests.get(enter_url)
     content = response.content
     soup = bs(content, 'html.parser')
-    allcits = soup.find('p')
-    print(allcits.strip())
+    eachptag = soup.find_all('p')
+
+    each_citation = '' 
+
+    for citation in eachptag:
+        if citation.find('sup', class_='noprint Inline-Template Template-Fact'):
+            eachcit = citation.text.strip()
+            eachcitsplit = eachcit.split('[citation needed]')
+            # print(eachcitsplit)
+            for i in eachcitsplit:
+                each_citation += f'{i}\n'
+            # for i in eachcitsplit:
+            #     pass
+                # print(f'Citation needed for {i}')
+                # each_citation += f'Citation needed for {i}'
+    print(each_citation)            
+    return each_citation
+    
     
 
 
